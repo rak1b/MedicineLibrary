aebcb3cab4e6342de0acbc31b4cce523c5ad52c3
rak1b <rak13.dev@gmail.com>
2021-09-16 23:20:54 +0600

    'details_added'

--- App/urls.py
+++ App/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import homeView
+from .views import homeView,detailsView
 urlpatterns = [
-    path('',homeView,name='homeView')
+    path('',homeView,name='homeView'),
+    path('details/<uuid:id>',detailsView,name='detailsView'),
+    
 ]
 
 if settings.DEBUG:
--- App/views.py
+++ App/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Medicine
+from django.core.paginator import Paginator
+
 # Create your views here.
+
+
 def homeView(request):
-    medicines=Medicine.objects.all().order_by('-name')
-    return render(request,'index.html',{'medicines':medicines})
+    medicines = Medicine.objects.all().order_by('name')
+    paginator = Paginator(medicines,6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj
+    }
+    return render(request, 'index.html', context)
+
+def detailsView(request,id):
+    print(request)
+    medicine = Medicine.objects.get(id=id)
+    medicine = get_object_or_404(Medicine, pk=id)
+    print('Medicine-----',medicine)
+
+    return render(request,'details.html',{'medicine':medicine})
